d78c24fcff4924b64087b1c569be0c59b0b1162f
Jahiker Rojas <52946797+Jahiker@users.noreply.github.com>
2021-01-17 19:15:50 -0400

    ajax request de busqueda e impresion de resultados de busqueda

--- js/_main.js
+++ js/_main.js
@@ -1,5 +1,30 @@
 $(function() {
 
+    // Getting the Shows Cards Container
+    var tvShowsContainer = $('#app-body').find('.tv-shows')
+
+    // Spinner loader animation
+    var spinner = '<div class="loader">Loading...</div>'
+    tvShowsContainer.append(spinner)
+
+    // Funcion para renderizar las cards de los shows
+    function renderShows(shows) {
+        shows.forEach(show => {
+
+            // Replacing values from the data
+            var article = template
+                .replace(':name:', show.name)
+                .replace(':img:', show.image.medium)
+                .replace(':img alt:', show.name)
+                .replace(':summary:', show.summary)
+
+            // Printing every show on inside the div with id="app-body"
+            var $article = $(article)
+            $article.hide();
+            tvShowsContainer.append($article.fadeIn(1500))
+        });
+    }
+
     // Submit Search Form
     $('#app-body')
         .find('.search-form')
@@ -13,6 +38,31 @@ $(function() {
                 .find('input[type="text')
                 .val()
 
+            // First remove all the shows
+            tvShowsContainer.find('.tv-show').remove()
+
+            // Showing spinner loader animation
+            tvShowsContainer.append(spinner)
+
+            // Getting shows by search
+            $.ajax('http://api.tvmaze.com/search/shows', {
+                data: { q: busqueda },
+                success: function(res, textStatus, xhr) {
+
+                    // Modificando la respuesta para solo mostrar los shows
+                    var shows = res.map((el) => {
+                        return el.show;
+                    })
+
+                    // Setting Timeout for making a loading effect
+                    setTimeout(() => {
+                        tvShowsContainer.find('.loader').remove()
+                        renderShows(shows)
+                    }, 1000);
+
+                }
+            })
+
         })
 
     var template = '<article class="tv-show">' +
@@ -25,25 +75,16 @@ $(function() {
         '</div>' +
         '</article>';
 
-    // Ajax Request
+    // Ajax Request Shows Collection
     $.ajax('http://api.tvmaze.com/shows', {
         success: function(shows, textStatus, xhr) {
 
-            // Getting the Shows Cards Container
-            var tvShowsContainer = $('#app-body').find('.tv-shows')
+            // Removing the spinner loader animation
+            tvShowsContainer.find('.loader').remove()
 
-            shows.forEach(show => {
+            // Render shows
+            renderShows(shows)
 
-                // Replacing values from the data
-                var article = template
-                    .replace(':name:', show.name)
-                    .replace(':img:', show.image.medium)
-                    .replace(':img alt:', show.name)
-                    .replace(':summary:', show.summary)
-
-                // Printing every show on inside the div with id="app-body"
-                tvShowsContainer.append($(article))
-            });
         }
     })
 
